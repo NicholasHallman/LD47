2b73f373fc236b8528dd2ecc7d33eda26078a218
Nicholas Hallman <advanderar@gmail.com>
2020-10-02 19:49:00 -0400

    feat: finished compile script

--- compile.ps1
+++ compile.ps1
@@ -1,2 +1,2 @@
 
-python3 ./picotool/p8tool build purgator.p8 --lua ./src/main.lua --gfx ./src/Sprites.p8 --sfx ./src/sfx.p8 --map ./src/Map.p8
+python3 ./picotool/p8tool build purgator.p8 --lua ./src/main.lua --gfx ./src/Assets.p8 --gff ./src/Assets.p8 --sfx ./src/Assets.p8 --map ./src/Assets.p8
