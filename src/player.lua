d97c4ae9bd380bdcaf0c43d105fd9902ffb15b17
Nicholas Hallman <advanderar@gmail.com>
2020-10-02 22:06:41 -0400

    added player and map

---
+++ src/player.lua
@@ -0,0 +1,11 @@
+
+player = {
+    x = 0,
+    y = 0,
+    jumpForce = 0,
+    isJumping = false,
+    speed = 4,
+    strength = 2,
+    doubleSwordEnabled = false,
+    doubleJumpEnabled = false,
+}
