a6fc35513d32a599a3a331d74ad4c5aaba5d1c19
Nicholas Hallman <advanderar@gmail.com>
2020-10-03 15:01:38 -0400

    feat: ground detection

--- src/player.lua
+++ src/player.lua
@@ -20,11 +20,11 @@ function make_player()
     gravity = 9.8 / 60, 
     down_force = 0,
     terminal_velocity = 5,
-    is_touching_ground = false,
+    is_touching_ground = true,
 
     
     draw = function (self)
-      spr(self.frames.standing, 64, 64)
+      spr(self.frames.standing, self.x + 64, self.y + 64)
     end,
     
     update = function (self)
@@ -34,16 +34,25 @@ function make_player()
       if(btn(right)) then 
         self.x = self.x + 1 
       end
-      if not is_touching_ground then 
+      if not self.is_touching_ground then 
         self.y += self.down_force
         self.down_force += self.gravity
         if self.down_force > self.terminal_velocity then
           self.down_force = self.terminal_velocity
         end
       end
-      if is_touching_ground then
+      if self.is_touching_ground then
         self.down_force = 0
+        stuck = world:is_touching_solid({x = (self.x + 4), y = (self.y + 7)})
+        if(stuck) then
+          self.y -= 1
+          printh('stuck')
+        end
       end
+    end,
+
+    is_wall = function(self) 
+      world:is_touching_solid({x = player.x, y = player.y})
     end
   }
   
--- src/world.lua
+++ src/world.lua
@@ -1,29 +1,33 @@
 
-
-world = {
-  non_solid_tiles = {},
-  
-  get_tile = function (x, y)
-    if(x > 128) then return -1 end
-    if( x < 0 ) then return -1 end
-    if( y > 64) then return -1 end
-    if( y < 0 ) then return -1 end
+function make_world ()
+  world = {
+    non_solid_tiles = {},
     
-    location = 0x2000 + ( x + (y * 128) )
-    return peek(location)
-  end,
-  
-  is_touching_solid = function (self, player)
-    on_tile = self.get_tile((player.x / 8), flr(player.y / 8))
-    if(on_tile > 191 and on_tile % 14 != 0 and on_tile % 15 != 0) then
-      return true
+    get_tile = function (x, y)
+      if(x > 128) then return -1 end
+      if( x < 0 ) then return -1 end
+      if( y > 64) then return -1 end
+      if( y < 0 ) then return -1 end
+      
+      location = 0x2000 + ( x + (y * 128) )
+      return peek(location)
+    end,
+    
+    is_touching_solid = function (self, player)
+
+      on_tile = self.get_tile(flr((player.x + 64) / 8), flr((player.y + 64) / 8))
+      printh('touching ' .. on_tile)
+      if(on_tile > 191) then
+        return true
+      end
+      return false
+    end,
+    
+    draw = function (self, player)
+      camera(player.x, player.y)
+      map(0, 0, 0, 0, 128, 32)
     end
-    return false
-  end,
-  
-  draw = function (self, player)
-    celx = flr(player.x / 8)
-    cely = flr(player.y / 8)
-    map(celx, cely, 0, 0, 16, 16)
-  end
-}
+  }
+
+  return world
+end
