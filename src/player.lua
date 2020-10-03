3d03c31bf9ed9e3e19820c5978c370a948929fca
BraydonKains <kainsbraydon@gmail.com>
2020-10-03 15:07:57 -0400

    player animations

--- src/player.lua
+++ src/player.lua
@@ -1,8 +1,10 @@
-
 left = 0
 right = 1
 up = 2
 down = 3
+sword_offset = 8
+player_x = 64
+player_y = 64
 
 function make_player()
   player = {
@@ -14,36 +16,98 @@ function make_player()
     strength = 2,
     double_sword_enabled = false,
     double_jump_enabled = false,
-    frames = {
-      standing = 0
-    },
     gravity = 9.8 / 60, 
     down_force = 0,
     terminal_velocity = 5,
     is_touching_ground = false,
 
-    
+    frames = {
+      stand = 0,
+      walk = 1,
+      jump = 2,
+      sword = {3, 4, 5, 6}
+    },
+    timers = {
+      jump = 0,
+      move = 0,
+      sword = 0
+    },
+    direction = right,
+    jump = false,
+    sword = false,
+    move = false, 
+
     draw = function (self)
-      spr(self.frames.standing, 64, 64)
+      self.character_frame(self)
+      if(self.sword) then self.sword_frame(self) end
     end,
     
     update = function (self)
       if(btn(left)) then 
         self.x = self.x - 1 
-      end
-      if(btn(right)) then 
+        move = true
+        direction = left
+      elseif(btn(right)) then 
         self.x = self.x + 1 
+        move = true
+        direction = right
+      else
+        move = false
+        self.timers.walk = 0
       end
-      if not is_touching_ground then 
+
+      if(btnp(4)) then self.jump = true end
+      if(btnp(5)) then self.sword = true end
+
+      if not self.is_touching_ground then 
         self.y += self.down_force
         self.down_force += self.gravity
         if self.down_force > self.terminal_velocity then
           self.down_force = self.terminal_velocity
         end
-      end
-      if is_touching_ground then
+      else
         self.down_force = 0
       end
+    end,
+
+    sword_frame = function (self) 
+      frame = self.frames.sword[1]
+      for i = 1, 4 do 
+        if(self.timers.sword < i*2) then
+          frame = self.frames.sword[i]
+          break
+        end
+      end
+
+      self.timers.sword += 1
+      if(self.timers.sword >= 8) then
+        self.timers.sword = 0
+        self.sword = false
+      end
+
+      flip = self.direction == self.right
+      sword_x = player_x - sword_offset
+      if(flip) sword_x = player_x + sword_offset
+      sword_y = player_y
+      spr(frame, sword_x, sword_y, 1, 1, flip, false)
+    end,
+
+    character_frame = function (self) 
+      frame = self.frames.stand
+      if(jump) then
+        frame = self.frames.jump
+        if(self.timers.jump>30) then
+          self.timers.jump = 0
+          jump = false
+        end
+      elseif(walk) then
+        if(self.timers.walk > 10) then s=1 end
+
+        self.timers.walk += 1
+        if(self.timers.walk>=20) then self.timers.walk=0 end
+      end 
+
+      spr(frame,self.x+64,self.y+64,1,1,self.direction==left,false)
     end
   }
   
